1041742cc56339d2319127ceb416fa52b36ade09
sean <j.sean1316@gmail.com>
2023-04-12 10:43:24 +0900

    board -> notice-view '공지사항' rename

--- src/main/java/kr/prt/partners/controllers/NoticeViewController.java
+++ src/main/java/kr/prt/partners/controllers/NoticeViewController.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class BoardController {
+public class NoticeViewController {
 
     private NoticeSerivce noticeSerivce;
     @Autowired
-    public BoardController(NoticeSerivce noticeSerivce) {
+    public NoticeViewController(NoticeSerivce noticeSerivce) {
         this.noticeSerivce = noticeSerivce;
     }
 
 //    @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
 //    public void memberRegi(Locale locale, Model model, String userName, MemberDto dto) {
-    @GetMapping("/notice/list")
+    @GetMapping("/notice/view")
     public String DashboardRe(Model model) {
         List<Notice> boards = noticeSerivce.read();
         System.out.println(boards);
         model.addAttribute("boards", boards);
-        return "notice/list";
+        return "/notice/view";
     }
 
 }
